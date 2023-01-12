fc615046fa0fbdd8f207fd1c59482f70a4dc017e
Cameron <cameron.harrison1@hotmail.co.uk>
2023-01-12 15:29:07 +0000

    Delete connection.php

--- block2/model/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//  Database connections 
-	$servername = "lochnagar.abertay.ac.uk";
-	$username = "sql1700523";
-	$password = "mXWC8iyKMVXa";
-	$dbname = "sql1700523";
-	$conn = mysqli_connect($servername, $username, $password, $dbname) ;
-?>
